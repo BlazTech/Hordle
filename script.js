b3db4ae8f4e583edb8709d4bc58f0a1dcff34409
Blaz Stanojevic <blaz.stanojevic@gmail.com>
2023-01-08 22:56:23 +0100

    Add Hordle class

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 class Grid {
-    constructor(mainWord) {
+    constructor() {
         this.grid = document.getElementById("grid");
-        this.mainWord = mainWord;
-        this.string = "";
         this.listOfRows = [];
-        this.rowNumber = 0;
-        this.abc = "abcčćdđefghijklmnoprsštuvzžABCČĆDĐEFGHIJKLMNOPRSŠTUVZŽ";
-        this.state = "playing";
     }
     //pravljenje i dodavanje kvadrata u listu
-    createSquareObjects() {
+    createSquareObjects(rowNumber, columnNumber) {
         let tempList = [];
-        for (let i=0; i < 30; i++ ) {
+        for (let i=0; i < (rowNumber * columnNumber); i++ ) {
             const square = document.createElement("div");
             square.id = "#s" + String(i);
             square.classList.add("cell", "regularbox");
@@ -22,23 +17,56 @@ class Grid {
             this.grid.append(square);
             tempList.push(square);
         
-            if (tempList.length >= 5) {
+            if (tempList.length >= columnNumber) {
                 this.listOfRows.push(tempList);
                 tempList = [];
             }
         }        
     }
     //obnavljanje teksta u gridu
-    updateGridText() {
-        for (let i=0; i < 5; i++) {
-            if (i < this.string.length) {
-                const child = this.listOfRows[this.rowNumber][i].querySelector(".letter-square");
-                child.innerText = this.string[i].toUpperCase();
+    updateGridText(string, currentRowNumber, columnNumber) {
+        for (let i=0; i < columnNumber; i++) {
+            if (i < string.length) {
+                const child = this.listOfRows[currentRowNumber][i].querySelector(".letter-square");
+                child.innerText = string[i].toUpperCase();
             } else {
-                const child = this.listOfRows[this.rowNumber][i].querySelector(".letter-square");
+                const child = this.listOfRows[currentRowNumber][i].querySelector(".letter-square");
                 child.innerText = "";
             }
         }
+    }
+
+    //bojanje grida
+    changeGridColors(color, currentRowNumber, i) {
+        this.listOfRows[currentRowNumber][i].classList.remove("regularbox");
+        this.listOfRows[currentRowNumber][i].classList.add(color);
+    }
+
+}
+
+class Hordle {
+    constructor(grid, keyboard, mainWord) {
+        this.grid = grid;
+        this.keyboard = keyboard;
+        this.mainWord = mainWord;
+        this.abc = "abcčćdđefghijklmnoprsštuvzžABCČĆDĐEFGHIJKLMNOPRSŠTUVZŽ";
+        this.state = "playing";
+
+        this.rowNumber = 6;
+        this.columnNumber = 5;
+        this.currentRowNumber = 0;
+        
+        this.string = "";
+    }
+
+    //pravljenje i dodavanje kvadrata u listu
+    createGridSquareObjects() {
+        this.grid.createSquareObjects(this.rowNumber, this.columnNumber);
+    }
+
+    //obnavljanje teksta u gridu
+    updateGridText() {
+        this.grid.updateGridText(this.string, this.currentRowNumber, this.columnNumber);
     }
 
     alertWin() {
@@ -55,35 +83,45 @@ class Grid {
         
         if (listOfWords.includes(this.string)) {
             //provjera slova na istom mjestu i slova koja nedostaju
-            for (let i=0; i < 5; i++) {
+            for (let i=0; i < this.columnNumber; i++) {
                 if (this.mainWord.includes(this.string[i])) {
                     if (this.mainWord[i] == this.string[i]) {
                         stringLetters[i] = "0";
                         mainWordLetters[i] = "1";
-                        this.listOfRows[this.rowNumber][i].classList.remove("regularbox");
-                        this.listOfRows[this.rowNumber][i].classList.add("greenbox");
+                        
+                        this.grid.changeGridColors("greenbox", this.currentRowNumber, i);
+
+                        //this.listOfRows[this.currentRowNumber][i].classList.remove("regularbox");
+                        //this.listOfRows[this.currentRowNumber][i].classList.add("greenbox");
                     }   
                 } else {
-                    this.listOfRows[this.rowNumber][i].classList.remove("regularbox");
-                    this.listOfRows[this.rowNumber][i].classList.add("darkgreybox");
+                    this.grid.changeGridColors("darkgreybox", this.currentRowNumber, i);
+
+                    //this.listOfRows[this.currentRowNumber][i].classList.remove("regularbox");
+                    //this.listOfRows[this.currentRowNumber][i].classList.add("darkgreybox");
                 }
             } 
             //provjera slova koja su prisutna, ali na različitom mjestu
-            for (let i=0; i < 5; i++) {
+            for (let i=0; i < this.columnNumber; i++) {
                 if (mainWordLetters.includes(stringLetters[i])) {
                     mainWordLetters[mainWordLetters.indexOf(stringLetters[i])] = "1";
-                    this.listOfRows[this.rowNumber][i].classList.remove("regularbox");
-                    this.listOfRows[this.rowNumber][i].classList.add("orangebox");
+
+                    this.grid.changeGridColors("orangebox", this.currentRowNumber, i);
+
+                    //this.listOfRows[this.currentRowNumber][i].classList.remove("regularbox");
+                    //this.listOfRows[this.currentRowNumber][i].classList.add("orangebox");
                 } else if (stringLetters[i] !== "0"){
-                    this.listOfRows[this.rowNumber][i].classList.remove("regularbox");
-                    this.listOfRows[this.rowNumber][i].classList.add("darkgreybox");
+                    this.grid.changeGridColors("darkgreybox", this.currentRowNumber, i);
+
+                    //this.listOfRows[this.currentRowNumber][i].classList.remove("regularbox");
+                    //this.listOfRows[this.currentRowNumber][i].classList.add("darkgreybox");
                 }
             }
             //provjera pobjede, gubitka
-            this.rowNumber += 1;
+            this.currentRowNumber += 1;
             if (this.string == this.mainWord) {
                 this.state = "win";
-            } else if (this.rowNumber > 5) {
+            } else if (this.currentRowNumber > 5) {
                 this.state = "lose";
             }
             //slanje poruka za pobjedu, gubitak
@@ -104,7 +142,7 @@ class Grid {
     inputCheck(event) {
         let key = event.key;
         if (this.abc.includes(key)) {
-            if (this.string.length < 5) {
+            if (this.string.length < this.columnNumber) {
                 this.string += key.toLowerCase();
                 this.updateGridText();
             }
@@ -112,26 +150,27 @@ class Grid {
             this.string = this.string.slice(0, this.string.length - 1);
             this.updateGridText();
         } else if (key == "Enter") {
-            if (this.string.length == 5) {
+            if (this.string.length == this.columnNumber) {
                 this.checkWord();
             }
         }
     }
 }
 
-function chooseMainWord() {
-    function randomInt(max) {
-        return Math.floor(Math.random() * (max + 1));
-    }
-    const mainWord = listOfWords[randomInt(listOfWords.length - 1)];
-    return mainWord;
-}
+// function chooseMainWord() {
+//     function randomInt(max) {
+//         return Math.floor(Math.random() * (max + 1));
+//     }
+//     const mainWord = listOfWords[randomInt(listOfWords.length - 1)];
+//     return mainWord;
+// }
 
-const grid = new Grid(chooseMainWord());
-grid.createSquareObjects();
+const grid = new Grid();
+const hordle = new Hordle(grid, null, "kanta");
+hordle.createGridSquareObjects();
 
 document.addEventListener("keydown", event => {
-    grid.inputCheck(event);
+    hordle.inputCheck(event);
 })
 
-console.log(grid.mainWord);
+console.log(hordle.mainWord);
